b777eff52bac5d51156bc74367f0fbe767913565
flo <florian.mouchard@sfr.fr>
2018-07-16 11:05:05 +0200

    Modification vue about, index, ViewStart et ajout class user dans model

---
+++ Roomy/Roomy/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Roomy.Models
+{
+    public class User
+    {
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Mail { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmePassword { get; set; }
+    }
+}
