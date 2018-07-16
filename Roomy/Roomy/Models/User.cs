95d1e7f7425f1a8c60fb3ee8a7bdbf01238e6c46
flo <florian.mouchard@sfr.fr>
2018-07-16 17:05:29 +0200

    Class User.cs, ajout required, regex, validation Model.

--- Roomy/Roomy/Models/User.cs
+++ Roomy/Roomy/Models/User.cs
@@ -10,12 +10,18 @@ namespace Roomy.Models
     {
         [Required(ErrorMessage = "Le champ nom est obligatoire")]
         [Display(Name = "Nom")]
+        [StringLength(50, MinimumLength = 2,
+            ErrorMessage = "Le champ {0} doit contenir entre {2} et {1} caractères")]
         public string LastName { get; set; }
 
         [Display(Name = "Prénom")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Le champ nom est obligatoire")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                           @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                           @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+            ErrorMessage = "Le format du mail est incorrect.")]
         [Display(Name = "Mail")]
         public string Mail { get; set; }
 
@@ -26,10 +32,13 @@ namespace Roomy.Models
 
         [Required(ErrorMessage = "Le champ nom est obligatoire")]
         [Display(Name = "Mot de passe")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{6,}$",
+            ErrorMessage = "Le format du mot de passe est incorrect, 6 caractères minimum, 1 lettre minuscule, 1 lettre majuscule, 1 chiffre et 1 caractère spécial")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Display(Name = "Confirmation du mot de passe")]
+        [Compare("Password", ErrorMessage = "Erreur sur la confirmation du mot de passe")]
         [DataType(DataType.Password)]
         public string ConfirmePassword { get; set; }
     }
