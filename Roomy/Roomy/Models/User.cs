10d604d0e16beb6204a7b1dd85eb07380e9fd3cd
flo <florian.mouchard@sfr.fr>
2018-07-16 16:04:24 +0200

    Modification Layout.cshtml, roomy.csproj, user.cs et ajout de create.cshtml, rommy.css, userController.cs.

--- Roomy/Roomy/Models/User.cs
+++ Roomy/Roomy/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,16 +8,29 @@ namespace Roomy.Models
 {
     public class User
     {
+        [Required(ErrorMessage = "Le champ nom est obligatoire")]
+        [Display(Name = "Nom")]
         public string LastName { get; set; }
 
+        [Display(Name = "Prénom")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Le champ nom est obligatoire")]
+        [Display(Name = "Mail")]
         public string Mail { get; set; }
 
+        [Required(ErrorMessage = "Le champ nom est obligatoire")]
+        [Display(Name = "Date de naissance")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "Le champ nom est obligatoire")]
+        [Display(Name = "Mot de passe")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Display(Name = "Confirmation du mot de passe")]
+        [DataType(DataType.Password)]
         public string ConfirmePassword { get; set; }
     }
 }
