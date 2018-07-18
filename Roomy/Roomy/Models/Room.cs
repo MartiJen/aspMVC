36bc783c55443b7bf7c45b3a02c7193dbb89237a
flo <florian.mouchard@sfr.fr>
2018-07-18 15:47:22 +0200

    Création d'une classe Room.

---
+++ Roomy/Roomy/Models/Room.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Roomy.Models
+{
+    public class Room : BaseModel
+    {
+        [Required(ErrorMessage ="Le champ {0} est obligatoire")]
+        [StringLength(50)]
+        [Display(Name = "Libellé")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [Display(Name = "Nombre de places")]
+        [Range(0, 50)]
+        public int capacity { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [Display(Name = "Tarif")]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [Display(Name = "Desciption")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [Display(Name = "Date de création")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{@:dddd dd MMMM yyyy")]
+        public DateTime CreatedAt { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire")]
+        [Display(Name = "Utilisateur")]
+        public int? UserID { get; set; }
+
+        [ForeignKey("UserID")]
+        public User Users { get; set; }
+    }
+}
